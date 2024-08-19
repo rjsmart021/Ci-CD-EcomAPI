e254b7ab7024f631a18233f4103913c4ba46f4b1
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-08-19 14:07:04 -0500

    Update product.py

--- New_EcomAPI /models/product.py
+++ New_EcomAPI /models/product.py
@@ -6,7 +6,7 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(db.String(100), nullable=False)
     price: Mapped[float] = mapped_column(nullable=False)
-    # TO-DO: BONUS: (1) view and manage stock levels, (2) restock products when low
+    #  BONUS:  view and manage stock levels, (2) restock products when low
     stock_quantity: Mapped[int] = mapped_column(nullable=False)
 
     def __repr__(self):

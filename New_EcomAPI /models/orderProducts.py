728afb1b97386c3589a1b7fcb260e7229c09cdc4
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-10-22 07:29:26 -0500

    Delete New_EcomAPI  directory

--- New_EcomAPI /models/orderProducts.py
+++
@@ -1,8 +0,0 @@
-from database import db
-
-order_products = db.Table(
-    "order_products",
-    db.Column('order_id', db.ForeignKey('orders.id'), primary_key=True),
-    db.Column('product_id', db.ForeignKey('products.id'), primary_key=True),
-    db.Column('quantity', db.Integer)
-)
--- New_EcomAPI /routes/loginBP.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from controllers.customerController import login
-
-login_blueprint = Blueprint("login_bp", __name__)
-
-login_blueprint.route('/', methods=['POST'])(login)
